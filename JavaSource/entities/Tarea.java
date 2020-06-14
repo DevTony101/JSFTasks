3f2cb666a1102b27c388a4dc60644d1ba8094a17
DevTony101 <tonycortesano@gmail.com>
2020-06-14 18:16:44 -0500

    Adding entities

---
+++ JavaSource/entities/Tarea.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tarea implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="title", length=25, nullable=false)
+	private String titulo;
+	
+	@Column(name="description", length=100)
+	private String descripcion;
+	
+	@Column(name="completed", nullable=false)
+	private Boolean completada;
+	
+	@Column(name="deadlineDate", nullable=false)
+	@Temporal(TemporalType.DATE)
+	private Date fechaFinal;
+
+}
---
+++ JavaSource/entities/Usuario.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="username", length=25, nullable=false)
+	private String nombreUsuario;
+	
+	@Column(name="firstName", length=50, nullable=false)
+	private String nombre;
+	
+	@Column(name="lastName", length=50, nullable=false)
+	private String apellido;
+	
+	@Column(name="civilStatus")
+	private String status;
+	
+	@Column(name="verified", nullable=false)
+	private Boolean verificado;
+	
+	@Column(name="created_at", nullable=false)
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacion;
+}
