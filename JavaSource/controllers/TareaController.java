5924db5d9810d0064f14cfc0d0f24ec942654223
DevTony101 <tonycortesano@gmail.com>
2020-06-17 15:58:47 -0500

    Finishing logic for creating new entity for filling it up when creating a new registry

--- JavaSource/controllers/TareaController.java
+++ JavaSource/controllers/TareaController.java
@@ -5,23 +5,38 @@ import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import org.jboss.logging.Logger;
+
+import entities.Tarea;
+import entities.Usuario;
+import lombok.Getter;
 import lombok.Setter;
+import services.TareaService;
+import services.UsuarioService;
 
 @Named
 @RequestScoped
 @Setter
+@Getter
 public class TareaController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static Logger LOG = Logger.getLogger(TareaController.class);
+	
+	private UsuarioService uService = UsuarioService.getInstance();
+	private TareaService tService = TareaService.getInstance();
+	private Tarea tarea = new Tarea();
+	
+	// Variable auxiliar para el nombre del usuario
+	private String usuario;
 
-	private String mensaje;
-
-	public TareaController() {
-		this.mensaje = "Hola Mundo";
-	}
-
-	public String getMensaje() {
-		this.mensaje += String.format(", desde %s", this.getClass().getName());
-		return this.mensaje;
+	public void guardarTarea() {
+		LOG.info(String.format("Titulo de la Tarea: %s", this.tarea.getTitulo()));
+		LOG.info(String.format("Descripcion de la Tarea: %s", this.tarea.getDescripcion()));
+		LOG.info(String.format("Dueño de la Tarea: %s", this.usuario));
+		Usuario usuario = uService.getByNombre(this.usuario.trim());
+		tService.save(this.tarea, usuario);
+		this.usuario = "";
+		this.tarea = new Tarea();
 	}
 }
--- JavaSource/controllers/UsuarioController.java
+++ JavaSource/controllers/UsuarioController.java
@@ -5,24 +5,31 @@ import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import org.jboss.logging.Logger;
+
+import entities.Usuario;
+import lombok.Getter;
 import lombok.Setter;
+import services.UsuarioService;
 
 @Named
 @RequestScoped
+@Getter
 @Setter
 public class UsuarioController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static Logger LOG = Logger.getLogger(UsuarioController.class);
 	
-	private String mensaje;
+	private UsuarioService uService = UsuarioService.getInstance();
+	private Usuario usuario = new Usuario();
 	
-	public UsuarioController() {
-		this.mensaje = "Hola Mundo";
+	public void guardarUsuario() {
+		LOG.info(String.format("Nombre: %s", this.usuario.getNombre()));
+		LOG.info(String.format("Apellido: %s", this.usuario.getApellido()));
+		LOG.info(String.format("Status Civil: %s", this.usuario.getStatus()));
+		LOG.info(String.format("Nombre de Usuario: %s", this.usuario.getNombreUsuario()));
+		uService.save(this.usuario);
+		this.usuario = new Usuario();
 	}
-	
-	public String getMensaje() {
-		this.mensaje += String.format(", desde %s", this.getClass().getName());
-		return this.mensaje;
-	}
-
 }
